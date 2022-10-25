c1a421771c12abce8c043b6b0b3445f3dcc3b3e0
dducnv0712 <ducbe2k2@gmail.com>
2022-10-26 00:03:38 +0700

    db-config

--- config/database.js
+++ config/database.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
@@ -5,9 +6,11 @@ module.exports = ({ env }) => ({
       host: env('DATABASE_HOST', 'ap-southeast.connect.psdb.cloud'),
       port: env.int('DATABASE_PORT', 3306),
       database: env('DATABASE_NAME', 'my-memories-db'),
-      user: env('DATABASE_USERNAME', 'wmtv3z3eqvfzu0iadcpe'),
+      user: env('DATABASE_USERNAME', '1vqsz4ob66uu32f92kh7'),
       password: env('DATABASE_PASSWORD', 'pscale_pw_HXLEk4vTKi4PxHvzYNCNKbsNq4UBRbcW4noYySNdsrm'),
-      ssl: env.bool('DATABASE_SSL', false),
+      ssl: {
+        ca: fs.readFileSync(__dirname + '/mysql-ca-main.crt')
+      },
     },
   },
 });
